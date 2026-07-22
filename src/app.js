0fc59e7849456cc0a7c3b64d9ef326f86a6a0a8d
Somraj <somrajdeb100@gmail.com>
2026-07-22 19:37:58 +0530

    Updated health route

--- src/app.js
+++ src/app.js
@@ -6,20 +6,26 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:"https://interview-prep-frontend-ashy.vercel.app",
+  origin: "https://interview-prep-frontend-ashy.vercel.app",
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
+
+// Health Route
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running",
+    timestamp: new Date().toISOString()
+  });
+})
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
 /* using all the routes here */
-app.use("/api/auth",authRouter)
-app.use("/api/interview",interviewRouter)
-
-
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
 
 module.exports = app
